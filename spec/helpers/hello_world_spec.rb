76132e09cb6369b115a192cacbc8ddb73f75ea7c
John Cusey <john_cusey@hotmail.com>
2019-09-09 13:38:08 +0000

    Rspec HelloWorld

---
+++ spec/helpers/hello_world_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../../app/models/hello_world'
+
+describe HelloWorld do 
+    it "should say 'Hello World' when we call the say_hello method" do 
+         hw = HelloWorld.new 
+         message = hw.say_hello 
+         expect(message).to eq "Hello World!"
+    end
+end
+
