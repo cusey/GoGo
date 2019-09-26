0ea9c9c1dfdfca4fb2bb5662a6b4f28ea0c33193
John Cusey <john_cusey@hotmail.com>
2019-09-26 12:41:03 +0000

    change controller

--- app/controllers/concern_module_controller.rb
+++ app/controllers/concern_module_controller.rb
@@ -2,10 +2,11 @@ class ConcernModuleController < ApplicationController
   
   include Unit::Priority
   
-
   def index
     @time = location_time
     @Profiles_List_1 = Profiles[1]
     @Profiles_List_1_1 = Profiles[1][1]
   end
+  
+  
 end
