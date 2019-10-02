b0a9801d5a71ab1ba73a2861495211de47e220a3
John Cusey <john_cusey@hotmail.com>
2019-10-02 20:02:36 +0000

    working upload

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,13 +1,16 @@
 class UploadController < ApplicationController
   def index
+    puts "****> uploads#index"
     
     @upload = Upload.new
 
   end
   
   def create 
+    puts "****> uploads#create"
   end
   
   def update
+    puts "****> uploads#update"
   end
 end
